32037f3a94433f05e93ee356b4969fac085f5b8d
kanishk <kanishkt@gmail.com>
2015-03-13 02:44:19 +0000

    second commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,55 @@
-var demoApp = angular.module('demoApp', []);
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-// demoApp.controller('demoController', ['$scope', '$http', function($scope, $http) {
-//   $http.get('public/data/cs498rk.json').success(function(data) {
-//     $scope.courseInfo = data;
-//     $scope.artistOrder = 'name';
-//   });
-// }]);
+phonecatControllers.controller('PhoneListCtrl',  ['$scope', '$http',
+    function ($scope, $http) {
+
+
+    $http.get('data/imdb250.json').success(function(data){
+        $scope.phones = data;
+    });
+
+
+    $scope.orderProp = 'rank';
+}]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$http', '$routeParams',
+    function($scope, $http, $routeParams) {
+        $http.get('data/imdb250.json').success(function(data){
+            $scope.phones = data;
+        });
+        $scope.rank = $routeParams.rank;
+    }]);
+
+
+phonecatControllers.controller('PhoneGalleryCtrl',  ['$scope', '$http',
+    function ($scope, $http) {
+
+
+    $http.get('data/imdb250.json').success(function(data){
+        $scope.phones = data;
+    });
+
+
+    $scope.orderProp = 'All';
+}]);
+
+phonecatControllers.controller('PhoneLangCtrl',  ['$scope', '$http',
+    function ($scope, $http) {
+
+
+    $http.get('data/imdb250.json').success(function(data){
+        $scope.phones = data;
+    });
+
+
+    var orderProp = $scope.orderProp = [];
+
+     $scope.updateSelection = function($event, id) {
+
+        var checkbox = $event.target;
+        var action = (checkbox.checked ? 'add' : 'remove');
+        if (action == 'add' & orderProp.indexOf(id) == -1) orderProp.push(id);
+        if (action == 'remove' && orderProp.indexOf(id) != -1) orderProp.splice(orderProp.indexOf(id), 1);
+    };
+
+}]);
